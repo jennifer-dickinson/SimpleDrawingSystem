451b4bcc665adea4e0a61cec45e206c901101bc4
jennifersalas <jennysalas@me.com>
2018-10-09 17:19:56 -0700

    Initial Commit

---
+++ SimpleDrawingSystem/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  SimpleDrawingSystem
+//
+//  Created by Jennifer Salas on 10/9/18.
+//  Copyright © 2018 Jennifer Salas. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
